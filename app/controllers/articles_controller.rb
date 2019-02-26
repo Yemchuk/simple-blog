b659305c41d6659ff5478ce07b28fd0440e53d4b
Oleh <emchuk86@gmail.com>
2019-02-26 21:50:55 +0200

    implement articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
-	def new
+
+  def show
+    @article = Article.find(params[:id])
+
+  end
+
+  def new
 
 	end
+
   def create
     @article = Article.new(article_params)
       if @article.valid?
