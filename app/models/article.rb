eadc70a94ae2ff8619c4b16c9d93ea51b142034b
Oleh <emchuk86@gmail.com>
2019-04-03 23:09:26 +0300

    add validation of length to article and comment models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
 	has_many :comments, dependent: :delete_all
   validates :title, :text, presence: true
+  validates :title, length: { maximum: 140 }
+  validates :text, length: { maximum: 4000 }
 
 
   def subject
