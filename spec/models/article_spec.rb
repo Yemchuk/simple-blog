346d0434e64e36852ab5c1e33c6ab8ccfec464f4
Oleh <emchuk86@gmail.com>
2019-04-03 19:00:01 +0300

    update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,10 +9,21 @@ describe Article do
     it { should have_many :comments }
   end
 
-  describe "subject" do
+  describe "#subject" do
     it "returns the article title" do
       article = create(:article, title: 'Lorem Ipsum')
       expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
+  describe "#last_comment" do
+    it "returns the last comment" do
+      #создаем статью, но с комментвми
+      article = create(:article_with_comments)
+
+      #проверка
+      expect(article.last_comment.body).to eq "comment body 3"
+
     end
   end
 end
