fb9307364cb24bc940196c83bc3905086db827bd
Oleh <emchuk86@gmail.com>
2019-04-03 23:10:03 +0300

    add test to article and comment spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,8 @@ describe Article do
   describe "Validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
+    it { should validate_length_of(:title).is_at_most(140)}
+    it { should validate_length_of(:text).is_at_most(4000)}
   end
   describe "associations" do
     it { should have_many :comments }
